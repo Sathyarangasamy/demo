609d9612d60177592170b8499cf7417d78a67472
S Rangasamy <S Rangasamy@DESKTOP-BS3CMF3>
2022-06-08 15:42:32 +0530

    first commit

---
+++ src/demo1/test1.java
@@ -0,0 +1,21 @@
+package demo1;
+
+public class test1 {
+	
+public static void main(String[] args)
+{
+String hungery = "yes";
+	
+	if(hungery=="yes")
+	{
+		System.out.println("eat somthing");
+	
+}
+	else
+	{
+		System.out.println("Do work");
+	}
+
+}
+}
+
